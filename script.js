2e4f71165047e2055b016e034e437e6a6dacceb4
1eonardo-byte <leonardoiglesiasmedina@gmail.com>
2025-12-18 11:37:29 +0100

    modificati i file logica, aggiunto il colore dello sfondo

--- script.js
+++ script.js
@@ -1,28 +1,46 @@
-
 console.log("Inizio esecuzione");
 let d = new Date();
 
-console.log(d);
-document.getElementById("saluto").innerHTML=d.getDay();
+
+let colori = [];
+
+document.getElementById("saluto").innerHTML = "Oggi è il " + d.getDay();
+
 
 function elabora() {
     let numero = document.getElementById("inNumber").value;
-    alert(numero);
+    
+    // serve per pulire sempre prima i messaggi, cancella quello che c'era prima
+    document.getElementById("outText").innerHTML = "";
+    document.getElementById("error").innerHTML = "";
 
-    if (numero >=0 && numero <= 255){
+    //se il n. è compreso tra 0 e 255
+    if (numero >= 0 && numero <= 255) {
+        // inserisce il numero dentro un array di colori 
+        colori.push(numero);
 
-        document.getElementById("outText").innerHTML = "hai inserito il numero" + numero;
-    } else{
-        //messaggio errore div error
-        document.getElementById("error").innerHTML = "il numero non è corretto"
-        //comportamento
+        document.getElementById("outText").innerHTML =
+            "Hai inserito il numero " + numero;
+        //visualizza la scritta + il numero scelto
+
+        //visualizza il colore scelto in base al numeno
+        document.getElementById("outText").innerHTML += "<br> i colori sono: " + colori;
+        console.log(colori);
+
+        if (colori.lemght == 3){
+            document.getElementById("outText").style.color = `rgd(${color[0]},${color[1]}, ${color[2]})`
+            
+        }
+
+    } else {
+        document.getElementById("error").innerHTML =
+            "Il numero non è corretto";
     }
-
-
 }
 
-function reset(){
+function reset() {
     document.getElementById("inNumber").value = "";
-    document.getElementById("outText").innerHTML= "Output"; //faccio il reset
-
-}
+    document.getElementById("outText").innerHTML = "";
+    document.getElementById("error").innerHTML = "";
+    colori = [];
+} 
